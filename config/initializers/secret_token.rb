8cdc79f5e26a772aaad023957cce2f6ddeb6cedb
Philipp Hofmann <hofmann.philipp@gmail.com>
2013-04-22 10:20:09 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FilterPlayer::Application.config.secret_token = 'a397af9c9fb903b369368322799c4b50000679df0697d66c005a2c064f6d38a3f05766dc479f0dd1ca912bdd1d3da5bc176e726a446bb3b84bca8235b6cdda0b'
